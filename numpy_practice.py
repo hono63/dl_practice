47da0e1bdf2351eecfe7cba3464f780e19079e24
hono63 <hono6363@yahoo.co.jp>
2020-06-02 00:28:22 +0900

    numpy practice

---
+++ numpy_practice.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+if __name__=="__main__":
+    x = np.array([1,2,3])
+    print(x.__class__)
+    print(x.shape)
+    print(x.ndim)
+
+    W = np.array([[1,2,3], [4,5,6]])
+    print(W.__class__)
+    print(W.shape)
+    print(W.ndim)
+
