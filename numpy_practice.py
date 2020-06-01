7e3cf973da7cf3c838e48b61116a16ced8cc2413
hono63 <hono6363@yahoo.co.jp>
2020-06-02 01:11:06 +0900

    plot

--- numpy_practice.py
+++ numpy_practice.py
@@ -1,7 +1,17 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__=="__main__":
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+def show_sigmoid():
+    x = np.arange(-5, 5, 0.1)
+    y = 1.0 / (1.0 + np.exp(-x)) # これでyもndarrayになる
+    plt.plot(x,y)
+    plt.show()
+
+def 基本():
     x = np.array([1,2,3])
     print(x.__class__)
     print(x.shape)
@@ -12,3 +22,34 @@ if __name__=="__main__":
     print(W.shape)
     print(W.ndim)
 
+    X = np.array([[0,1,2], [3,4,5]])
+    print(X+W)
+    print(X*W) # 要素ごとの掛け算になる
+    print(W*np.array([10,20,30])) # 勝手に拡張されてW * [[10,20,30],[10,20,30]] の掛け算になる ちょっとキモチワルイ
+    # ベクトルの内積
+    a = np.array([1,2,3])
+    b = np.array([4,5,6])
+    print(np.dot(a, b))
+    # 行列の積
+    A = np.array([[1,2],[3,4]])
+    B = np.array([[5,6],[7,8]])
+    print(np.dot(A, B))
+
+def all_connection():
+    W1 = np.random.randn(2,4)
+    b1 = np.random.randn(4)
+    W2 = np.random.randn(4,3)
+    b2 = np.random.randn(3)
+    x  = np.random.randn(10,2) # 10個のサンプルデータ
+    h  = np.dot(x,W1) + b1
+    #print(W1)
+    #print(b1)
+    #print(x)
+    print(h)
+    a  = sigmoid(h)
+    s  = np.dot(a,W2) + b2
+    print(s)
+
+if __name__=="__main__":
+    #show_sigmoid()
+    all_connection()
