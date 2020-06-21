9807e2f7f58451ac21cdf6ed5a15822085f48559
hono63 <hono6363@yahoo.co.jp>
2020-06-21 16:57:17 +0900

    comatrix

--- natual_language.py
+++ natual_language.py
@@ -13,18 +13,34 @@ from dataset import spiral
 config.GPU = True
 
 def preprocessing(text):
+    "コーパスを作成する"
     words = text.lower().replace(".", " .").split(" ")
     word_to_id = {}
     id_to_word = {}
     for word in words:
-        if word not in word_to_id:
-            new_id = str(uuid.uuid4())
+        if word not in word_to_id.keys():
+            #new_id = str(uuid.uuid4())
+            new_id = len(word_to_id)
             word_to_id[word] = new_id
-            id_to_word[new_id]   = word
-    corpus = np.array(list(id_to_word.keys()))
+            id_to_word[new_id] = word
+    corpus = np.array([word_to_id[word] for word in words])
     return corpus, word_to_id, id_to_word
 
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    "共起行列を作成する"
+    length = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+    for idx, word_id in enumerate(corpus):
+        for widx in range(max(idx-window_size, 0), min(idx+window_size+1, length)):
+            if widx == idx:
+                continue
+            word_id2 = corpus[widx]
+            co_matrix[word_id, word_id2] += 1
+    return co_matrix
+
 if __name__=="__main__":
-    corpus, word_to_id, id_to_word = preprocessing('You said goodbye and I say hello.')
-    print(corpus)
+    corpus, word_to_id, id_to_word = preprocessing('You say goodbye and I say hello.')
+    print(word_to_id.items())
+    co_matrix = create_co_matrix(corpus, len(word_to_id), window_size=1)
+    print(co_matrix)
     
